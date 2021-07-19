62b7a1c8cad1f02046c12d68d8b986d702a381fc
vitaoTM <vitor.grosskopf@gmail.com>
2021-07-19 14:59:37 -0300

    Atualizando codigo da aula 05

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 
 import React from 'react'
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -47,8 +49,8 @@ function ProfileRelationsBox(propriedades) {
 }
 
 
-export default function Home() {
-  const usuarioAleatorio = 'vitaoTM';
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser;
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'jvitorfromhell',
@@ -221,3 +223,30 @@ export default function Home() {
     </>
   )
 }
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut-vitaoTM.vercel.app/api/auth', {
+    headers: {
+        Authorization: token
+      }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, // will be passed to the page component as props
+  }
+} 
